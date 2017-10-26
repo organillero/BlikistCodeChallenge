98a9a8a919df47dbadc0ab22499a6df555908e3e
Carlos Ferreyra <carlos@beaconhome.io>
2017-10-26 17:18:07 -0500

    First commit

---
+++ app/src/main/java/io/ferreyra/blinkist_code_challege/LibraryActivity.java
@@ -0,0 +1,13 @@
+package io.ferreyra.blinkist_code_challege;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LibraryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_library);
+    }
+}
